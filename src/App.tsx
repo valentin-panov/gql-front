bf27ca7d1b18605e2281b53cfde59413c97757dd
Valentin Panov <valentin.panov@me.com>
2023-10-13 14:05:44 +0200

    run localhost

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import * as React from 'react';
 import { Test } from 'src/screens/Test';
+import Cookies from 'universal-cookie';
 import { Client } from './client';
 
-export const App: React.FC = () => (
-  <Client>
-    <Test />
-  </Client>
-);
+export const App: React.FC = () => {
+  const cookies = new Cookies(null, { path: '/', domain: '.lvh.me' });
+  // const cookies = new Cookies(null, { path: '/' });
+  cookies.set('XSRF-TOKEN', 'token', { sameSite: 'strict' });
+  return (
+    <Client>
+      <Test />
+    </Client>
+  );
+};
